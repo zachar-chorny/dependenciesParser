fe71c22c9ed1dfcdab08fc6563dc58b699652f8b
zchornyi <ufumoveldar@gmail.com>
2022-07-07 11:16:36 +0300

    InstructionServiceImpl fixed

--- src/main/java/com/example/parse/service/InstructionServiceImpl.java
+++ src/main/java/com/example/parse/service/InstructionServiceImpl.java
@@ -9,6 +9,7 @@ import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,48 +21,54 @@ public class InstructionServiceImpl implements InstructionService {
 
     @Override
     public List<Node> addNodes(List<Node> nodes, ProjectInstruction instruction) {
+        List<Node> changedNodes = new ArrayList<>(nodes);
         if (instruction != null) {
             List<DependencyNode> dependencyNodes = instruction.getNodesForAdding();
             if (dependencyNodes != null) {
                 for (DependencyNode dependencyNode : dependencyNodes) {
-                    getNode(dependencyNode).ifPresent(nodes::add);
+                    getNode(dependencyNode).ifPresent(changedNodes::add);
                 }
             }
+            return changedNodes;
         }
         return nodes;
     }
 
     @Override
     public List<Node> replaceNodes(List<Node> nodes, ProjectInstruction instruction) {
+        List<Node> changedNodes = new ArrayList<>(nodes);
         if (instruction != null) {
             List<DependencyNode> dependencyNodes = instruction.getNodesFroReplacing();
             if (dependencyNodes != null) {
                 for (DependencyNode dependencyNode : dependencyNodes) {
-                    for (int i = 0; i < nodes.size(); i++) {
-                        Node node = nodes.get(i);
+                    for (int i = 0; i < changedNodes.size(); i++) {
+                        Node node = changedNodes.get(i);
                         if (dependencyNode.getGroupId().equals(node.getGroupId()) &&
                                 dependencyNode.getArtifactId().equals(node.getArtifactId())) {
                             Optional<Node> optionalNode = getNode(dependencyNode);
                             if (optionalNode.isPresent()) {
-                                nodes.set(i, optionalNode.get());
+                                changedNodes.set(i, optionalNode.get());
                             }
                         }
                     }
                 }
             }
+            return changedNodes;
         }
         return nodes;
     }
 
     @Override
     public List<Node> removeNodes(List<Node> nodes, ProjectInstruction instruction) {
+        List<Node> changedNodes = new ArrayList<>(nodes);
         if(instruction != null) {
             List<String> artifactIds = instruction.getArtifactIdsForRemoving();
             if (artifactIds != null) {
                 for (String artifactId : artifactIds) {
-                    nodes.removeIf(node -> artifactId.equals(node.getArtifactId()));
+                    changedNodes.removeIf(node -> artifactId.equals(node.getArtifactId()));
                 }
             }
+            return changedNodes;
         }
         return nodes;
     }
