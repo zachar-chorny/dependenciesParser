a7057d9c1133dbf795fc4ef3e7bf28a91aa2a59d
zchornyi <ufumoveldar@gmail.com>
2022-07-28 13:37:33 +0300

    multithreading added

--- src/main/java/com/example/parse/facade/ParseFacade.java
+++ src/main/java/com/example/parse/facade/ParseFacade.java
@@ -1,5 +1,6 @@
 package com.example.parse.facade;
 
+import com.example.parse.model.CallableTask;
 import com.example.parse.model.Project;
 import com.example.parse.model.ProjectInstruction;
 import com.example.parse.service.ParseService;
@@ -10,10 +11,15 @@ import org.apache.maven.model.Model;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Service
 @AllArgsConstructor
@@ -23,17 +29,41 @@ public class ParseFacade {
     private final ProjectResolveService projectResolveService;
 
     public List<Project> createProjectsFromFile(File file, boolean resolveDependencies) {
+        List<File> files = getFiles(file);
+        return getProjectsFromFiles(files, resolveDependencies);
+    }
+
+    private List<Project> getProjectsFromFiles(List<File> files, boolean resolveDependencies) {
         List<Project> projects = new ArrayList<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(25);
+        List<Future<Optional<Project>>> futures = new ArrayList<>();
+        for (File iterFile : files) {
+            futures.add(executorService.submit(new CallableTask(parseService, projectService, iterFile,
+                    resolveDependencies)));
+        }
+        executorService.shutdown();
+        for (Future<Optional<Project>> future : futures) {
+            try {
+                future.get().ifPresent(projects::add);
+            } catch (InterruptedException | ExecutionException ignored) {
+            }
+        }
+        return projects;
+    }
+
+    private List<File> getFiles(File file) {
+        List<File> fileList = new ArrayList<>();
         if (file.isDirectory()) {
             File[] files = file.listFiles();
             if (files != null) {
-                Arrays.stream(files).forEach(f -> projects.addAll(createProjectsFromFile(f, resolveDependencies)));
+                Arrays.stream(files).forEach(f -> fileList.addAll(getFiles(f)));
             }
         } else {
-            parseService.getModelFromFile(file).ifPresent(model -> projects.add
-                    (projectService.createProjectFromModel(model, resolveDependencies)));
+            if(file.getName().equals("pom.xml")){
+                fileList.add(file);
+            }
         }
-        return projects;
+        return fileList;
     }
 
     public List<Project> createProjectsFromFile(File file, List<ProjectInstruction> instructions) {
---
+++ src/main/java/com/example/parse/model/CallableTask.java
@@ -0,0 +1,30 @@
+package com.example.parse.model;
+
+import com.example.parse.service.ParseService;
+import com.example.parse.service.ProjectService;
+import org.apache.maven.model.Model;
+
+import java.io.File;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+
+public class CallableTask implements Callable<Optional<Project>> {
+    private final ParseService parseService;
+    private final ProjectService projectService;
+    private final File file;
+    private final boolean resolveDependencies;
+
+    public CallableTask(ParseService parseService, ProjectService projectService, File file,
+                        boolean resolveDependencies){
+        this.parseService = parseService;
+        this.projectService = projectService;
+        this.file = file;
+        this.resolveDependencies = resolveDependencies;
+    }
+
+    @Override
+    public Optional<Project> call() {
+        Optional<Model> optionalModel = parseService.getModelFromFile(file);
+        return optionalModel.map(m -> projectService.createProjectFromModel(m, resolveDependencies));
+    }
+}
